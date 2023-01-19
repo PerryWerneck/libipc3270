688e4c42c625c2fe03479ef1beda06fff1e552d3
Perry Werneck <perry.werneck@gmail.com>
2023-01-19 09:03:05 -0300

    Adjustments in win32 ci-build.

--- win/ci-build.sh
+++ win/ci-build.sh
@@ -7,14 +7,6 @@
 #
 echo "Running ${0}"
 
-LOGFILE=build.log
-rm -f ${LOGFILE}
-
-die ( ) {
-	[ "$1" ] && echo "$*"
-	exit -1
-}
-
 myDIR=$(dirname $(dirname $(readlink -f ${0})))
 
 cd ${myDIR}
@@ -35,22 +27,9 @@ make install || die "Lib3270 Install failure"
 cd ../..
 
 #
-# Build LIBV3270
+# Build LIBIPC3270
 #
-echo "Building libv3270"
-git clone https://github.com/PerryWerneck/libv3270.git ./.build/libv3270 || die "clone libv3270 failure"
-cd ./.build/libv3270
-./autogen.sh || die "Libv3270 Autogen failure"
-./configure || die "Libv3270 Configure failure"
-make clean || die "Libv3270 Make clean failure"
-make all || die "Libv3270 Make failure"
-make install || die "Libv3270 Install failure"
-cd ../..
-
-#
-# Build IPC3270
-#
-echo "Building IPC"
+echo "Building LIBIPC3270"
 ./autogen.sh || die "Autogen failure"
 ./configure || die "Configure failure"
 make clean || die "Make clean failure"
