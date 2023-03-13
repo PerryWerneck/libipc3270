e9b7c190da798d163942f32a58ba7b8f2a25f24e
Perry Werneck <perry.werneck@gmail.com>
2023-03-12 21:52:52 -0300

    Unpacking lib3270 if available.

--- win/ci-build.sh
+++ win/ci-build.sh
@@ -16,15 +16,24 @@ mkdir -p ./.build
 #
 # Build LIB3270
 #
-echo "Building lib3270"
-git clone https://github.com/PerryWerneck/lib3270.git ./.build/lib3270 || die "clone lib3270 failure"
-cd ./.build/lib3270
-./autogen.sh || die "Lib3270 autogen failure"
-./configure || die "Lib3270 Configure failure"
-make clean || die "Lib3270 Make clean failure"
-make all || die "Lib3270 Make failure"
-make install || die "Lib3270 Install failure"
-cd ../..
+find . -iname *.tar.xz
+
+if [ -e mingw-lib3270.tar.xz ]; then
+
+	echo "Unpacking lib3270"
+	tar -C / -Jxvf mingw-lib3270.tar.xz 
+
+else
+	echo "Building lib3270"
+	git clone https://github.com/PerryWerneck/lib3270.git ./.build/lib3270 || die "clone lib3270 failure"
+	cd ./.build/lib3270
+	./autogen.sh || die "Lib3270 autogen failure"
+	./configure || die "Lib3270 Configure failure"
+	make clean || die "Lib3270 Make clean failure"
+	make all || die "Lib3270 Make failure"
+	make install || die "Lib3270 Install failure"
+	cd ../..
+fi
 
 #
 # Build LIBIPC3270
